ef01c19724f0382e9c03e734e2f480cdf6c18920
ubervelocity <mihaitu.popescu@gmail.com>
2019-03-31 16:23:38 +0200

    almost done

--- roundrobin.c
+++ roundrobin.c
@@ -77,7 +77,6 @@ void insertIntoQueue(Process *p, Queue *q) {
         }
     }
     q->slots[q->back] = *p;
-    q->members++;
     q->back++;
 }
 
@@ -169,7 +168,7 @@ void populateProcess(Process *process, int *values) {
     process->arrivalTime = values[0];
     process->priority = values[1];
     process->end = values[i + 2];
-    process->times = realloc(process->times, (i+1) * sizeof(int));
+    // process->times = realloc(process->times, (i+1) * sizeof(int));
 }
 
 /**
@@ -190,15 +189,17 @@ void print2dIntArray(int **array, int numProcesses) {
 /**
  * Initialization of queue of specific size.
  */
-Queue initializeQueue(int size, int priority) {
+Queue initializeQueue(int size, int priority, int members) {
     Queue q;
     q.priority = priority;
     q.size = size;
     q.time = 0;
-    q.members = 0;
+    q.members = members;
+    q.processPointer = 0;
     q.front = 0;
     q.back = 0;
-    q.slots = malloc(size * sizeof(Process));
+    if (size == 0)  q.slots = malloc(MAX_NUM_PROCESSES * sizeof(Process));
+    else q.slots = malloc(size * sizeof(Process));
     assert(q.slots != NULL);
     return q;
 }
@@ -387,36 +388,311 @@ void populateQueues(int **sortedArray, Queue *lowQueue, Queue *medQueue, Queue *
     }
 }
 
+/**
+ * Checks which process arrives first from the three queues.
+ * Use the time at which it arrives to update the clock.
+ * Used only when all processes at the pointers are doing I/O
+ * so there are no runnable processes.
+ */
+int fastForward(Queue *lowQueue, Queue *medQueue, Queue *highQueue) {
+    int min = MAX;
+    if (lowQueue->members > 0 && min > lowQueue->slots[lowQueue->processPointer].arrivalTime) {
+        min = lowQueue->slots[lowQueue->processPointer].arrivalTime;
+    }
+    if (medQueue->members > 0 && min > medQueue->slots[medQueue->processPointer].arrivalTime) {
+        min = medQueue->slots[medQueue->processPointer].arrivalTime;
+    }
+    if (highQueue->members > 0 && min > highQueue->slots[highQueue->processPointer].arrivalTime) {
+        min = highQueue->slots[highQueue->processPointer].arrivalTime;
+    }
+    return min;
+
+}
+
+int allQueuesFinished(Queue *lowQueue, Queue *medQueue, Queue *highQueue) {
+    return (processesHaveFinished(lowQueue, lowQueue->members) && processesHaveFinished(medQueue, medQueue->members) && processesHaveFinished(highQueue, highQueue->members));
+}
+
+void roundRobinPriority(Queue *lowQueue, Queue *medQueue, Queue *highQueue) {
+    int pclock = 0;
+    while (!allQueuesFinished(lowQueue, medQueue, highQueue)) {
+        // Check if the high-priority process at pointer position
+        // has arrived and is ready to be run.
+        if (highQueue->members > 0 && highQueue->slots[highQueue->processPointer].arrivalTime <= pclock && !processesHaveFinished(highQueue, highQueue->members)) {
+            printf("working on high - pv:%d\tclock:%d\n", highQueue->slots[highQueue->processPointer].pointer, pclock);
+            // Update time.
+            if (highQueue->slots[highQueue->processPointer].
+            times[highQueue->slots[highQueue->processPointer].pointer] >= QUANTUM) {
+                pclock += QUANTUM;
+            }
+            else {
+                pclock += highQueue->slots[highQueue->processPointer].
+                times[highQueue->slots[highQueue->processPointer].pointer];
+            }
+            highQueue->slots[highQueue->processPointer].
+            times[highQueue->slots[highQueue->processPointer].pointer] -= QUANTUM;
+            // If we finished a CPU burst, two options: finished or I/O.
+            if (highQueue->slots[highQueue->processPointer].
+            times[highQueue->slots[highQueue->processPointer].pointer] <= 0) {
+                // Pointer of processor operation gets incremented.
+                // (CAN LAND ON I/O OR -1).
+                highQueue->slots[highQueue->processPointer].pointer++;
+                // Process ended.
+                printf("pointer value with which we compare = %d\n", highQueue->slots[highQueue->processPointer].times[highQueue->slots[highQueue->processPointer].pointer]);
+                if (highQueue->slots[highQueue->processPointer].times[highQueue->slots[highQueue->processPointer].pointer] == -1) {
+                    highQueue->slots[highQueue->processPointer].finished = 1;
+                    // Compute completionTime and turnaroundTime.
+                    highQueue->slots[highQueue->processPointer].completionTime = pclock;
+                    highQueue->slots[highQueue->processPointer].turnaroundTime = highQueue->slots[highQueue->processPointer].completionTime - highQueue->slots[highQueue->processPointer].originalArrivalTime;
+                }
+                // I/O time
+                else {
+                    highQueue->slots[highQueue->processPointer].arrivalTime = pclock + highQueue->slots[highQueue->processPointer].times[highQueue->slots[highQueue->processPointer].pointer];
+                    highQueue->slots[highQueue->processPointer].pointer++;
+                }
+            }
+            // Update process pointer position for round-robin.
+            if (highQueue->processPointer == highQueue->members - 1) {
+                highQueue->processPointer = 0;
+            }
+            else {
+               
+                highQueue->processPointer++;
+
+            }
+            // We are not interested in processes that have finished, so first we check
+            // whether the whole queue has finished.
+            if (!processesHaveFinished(highQueue, highQueue->members)) {
+                // We go to the next process that has not finished within the queue.
+                while (highQueue->slots[highQueue->processPointer].finished == 1) {
+                    if (highQueue->processPointer == highQueue->members - 1) {
+                        highQueue->processPointer = 0;
+                    }
+                    else {
+                        highQueue->processPointer++;
+                    }
+                }
+            }
+        }
+
+        // MEDIUM QUEUE
+        else if (medQueue->members > 0 && medQueue->slots[medQueue->processPointer].arrivalTime <= pclock && !processesHaveFinished(medQueue, medQueue->members)) {
+            printf("working on med - pv:%d\tclock:%d\n", medQueue->slots[medQueue->processPointer].pointer, pclock);
+            // Update time.
+            if (medQueue->slots[medQueue->processPointer].
+            times[medQueue->slots[medQueue->processPointer].pointer] >= QUANTUM) {
+                pclock += QUANTUM;
+                medQueue->slots[medQueue->processPointer].
+                times[medQueue->slots[medQueue->processPointer].pointer] -= QUANTUM;
+            }
+            else {
+                pclock += medQueue->slots[medQueue->processPointer].
+                times[medQueue->slots[medQueue->processPointer].pointer];
+            }
+            medQueue->slots[medQueue->processPointer].
+            times[medQueue->slots[medQueue->processPointer].pointer] -= QUANTUM;
+            // If we finished a CPU burst, two options: finished or I/O.
+            if (medQueue->slots[medQueue->processPointer].
+            times[medQueue->slots[medQueue->processPointer].pointer] <= 0) {
+                // Pointer of processor operation gets incremented.
+                // (CAN LAND ON I/O OR -1).
+                medQueue->slots[medQueue->processPointer].pointer++;
+                printf("pointer value with which we compare = %d\n", medQueue->slots[medQueue->processPointer].times[medQueue->slots[medQueue->processPointer].pointer]);
+
+                // Check if process ended.
+                if (medQueue->slots[medQueue->processPointer].
+                times[medQueue->slots[medQueue->processPointer].pointer] == -1) {
+                    medQueue->slots[medQueue->processPointer].finished = 1;
+                    // Compute completionTime and turnaroundTime.
+                    medQueue->slots[medQueue->processPointer].completionTime = pclock;
+                    medQueue->slots[medQueue->processPointer].turnaroundTime = medQueue->slots[medQueue->processPointer].completionTime - medQueue->slots[medQueue->processPointer].originalArrivalTime;
+                }
+                // I/O time
+                else {
+                    medQueue->slots[medQueue->processPointer].arrivalTime = pclock + medQueue->slots[medQueue->processPointer].times[medQueue->slots[medQueue->processPointer].pointer];
+                    medQueue->slots[medQueue->processPointer].pointer++;                
+                }
+            }
+            // Update process pointer position for round-robin.
+            if (medQueue->processPointer == medQueue->members - 1) {
+                medQueue->processPointer = 0;
+            }
+            else {
+                medQueue->processPointer++;
+            }
+            // We are not interested in processes that have finished, so first we check
+            // whether the whole queue has finished.
+            if (!processesHaveFinished(medQueue, medQueue->members)) {
+                // We go to the next process that has not finished within the queue.
+                while (medQueue->slots[medQueue->processPointer].finished == 1) {
+                    if (medQueue->processPointer == medQueue->members - 1) {
+                        medQueue->processPointer = 0;
+                    }
+                    else {
+                        medQueue->processPointer++;
+                    }
+                }
+            }
+        }
+
+        // LOW QUEUE
+        else if (lowQueue->members > 0 && lowQueue->slots[lowQueue->processPointer].arrivalTime <= pclock && !processesHaveFinished(lowQueue, lowQueue->members)) {
+        // Update time.
+            printf("working on low - pv:%d\tclock:%d\n", lowQueue->slots[lowQueue->processPointer].pointer, pclock);
+            if (lowQueue->slots[lowQueue->processPointer].
+            times[lowQueue->slots[lowQueue->processPointer].pointer] >= QUANTUM) {
+                pclock += QUANTUM;
+                lowQueue->slots[lowQueue->processPointer].
+                times[lowQueue->slots[lowQueue->processPointer].pointer] -= QUANTUM;
+            }
+            else {
+                pclock += lowQueue->slots[lowQueue->processPointer].
+                times[lowQueue->slots[lowQueue->processPointer].pointer];
+            }
+            lowQueue->slots[lowQueue->processPointer].
+            times[lowQueue->slots[lowQueue->processPointer].pointer] -= QUANTUM;
+            // If we finished a CPU burst, two options: finished or I/O.
+            if (lowQueue->slots[lowQueue->processPointer].
+            times[lowQueue->slots[lowQueue->processPointer].pointer] <= 0) {
+                // Pointer of processor operation gets incremented.
+                // (CAN LAND ON I/O OR -1).
+                lowQueue->slots[lowQueue->processPointer].pointer++;
+                printf("pointer value with which we compare = %d\n", lowQueue->slots[lowQueue->processPointer].times[lowQueue->slots[lowQueue->processPointer].pointer]);
+
+                // Check if process ended.
+                if (lowQueue->slots[lowQueue->processPointer].
+                times[lowQueue->slots[lowQueue->processPointer].pointer] == -1) {
+                    lowQueue->slots[lowQueue->processPointer].finished = 1;
+                    // Compute completionTime and turnaroundTime.
+                    lowQueue->slots[lowQueue->processPointer].completionTime = pclock;
+                    lowQueue->slots[lowQueue->processPointer].turnaroundTime = lowQueue->slots[lowQueue->processPointer].completionTime - lowQueue->slots[lowQueue->processPointer].originalArrivalTime;
+                }
+                // I/O time
+                else {
+                    lowQueue->slots[lowQueue->processPointer].arrivalTime = pclock + lowQueue->slots[lowQueue->processPointer].times[lowQueue->slots[lowQueue->processPointer].pointer];
+                    lowQueue->slots[lowQueue->processPointer].pointer++;                
+                }
+            }
+            // Update process pointer position for round-robin.
+            if (lowQueue->processPointer == lowQueue->members - 1) {
+                lowQueue->processPointer = 0;
+            }
+            else {
+                lowQueue->processPointer++;
+            }
+            // We are not interested in processes that have finished, so first we check
+            // whether the whole queue has finished.
+            if (!processesHaveFinished(lowQueue, lowQueue->members)) {
+                // We go to the next process that has not finished within the queue.
+                while (lowQueue->slots[lowQueue->processPointer].finished == 1) {
+                    if (lowQueue->processPointer == lowQueue->members - 1) {
+                        lowQueue->processPointer = 0;
+                    }
+                    else {
+                        lowQueue->processPointer++;
+                    }
+                }
+            }
+        }
+
+        // All processes are doing I/O, so processor should wait for the
+        // first runnable process. If there are multiple finishing at the same time,
+        // they should be execited by priority.
+        else {
+            pclock = fastForward(lowQueue, medQueue, highQueue);
+        }
+    }
+}
+
+void countProcesses(int **sorted, int *l, int *m, int *h, int numProcesses) {
+    int i;
+    for (i = 0; i < numProcesses; i++) {
+        if (sorted[i][1] == 1) {
+            *h = *h + 1;
+        }
+        else if (sorted[i][1] == 2) {
+            *m = *m + 1;
+        }
+        else if (sorted[i][1] == 3) {
+            *l = *l + 1;
+        }
+    }
+}
+
+void printTotalAverage(Queue *low, Queue *med, Queue *high) {
+    int i, j;
+    float la = 0.0, ma = 0.0, ha = 0.0;
+    int count = 0;
+    if (low->members > 0) {
+        for (i = 0; i < low->members; i++) {
+            la += low->slots[i].turnaroundTime;
+        }
+        la /= low->members;
+    }
+    printf("la = %.2lf\n", la);
+    if (med->members > 0) {
+        for (i = 0; i < med->members; i++) {
+            ma += med->slots[i].turnaroundTime;
+        }
+        ma /= med->members;
+    }
+    printf("ma = %.2lf\n", ma);
+    
+    if (high->members > 0) {
+        for (i = 0; i < high->members; i++) {
+            ha += high->slots[i].turnaroundTime;
+        }
+        ha /= high->members;
+    }
+    
+    printf("ha = %.2lf\n", ha);
+
+    if (low->members > 0)   count++;
+    if (med->members > 0)   count++;
+    if (high->members > 0)  count++;
+
+    if (count > 0) {
+        printf("Total average = %.2lf\n", (la + ma + ha)/count);
+    }
+}
+
 int main(int argc, char* argv[]) {
     char **input;
-    int **intArray, numProcesses, i, j, **sortedArray, demo_mode = 0;
+    int **intArray, numProcesses, i, j, **sortedArray, demo_mode = 0, l = 0, m = 0, h = 0;
     if (argv[1] != NULL && strcmp(argv[1], "demo") == 0) {
         demo_mode = 1;
     }
     input = readInput(&numProcesses);
     intArray = convertStrArr(input, numProcesses);
     sortedArray = sort2DArray(intArray, numProcesses);
-    Queue lowQueue = initializeQueue(numProcesses, P_LOW);
-    Queue medQueue = initializeQueue(numProcesses, P_MEDIUM);
-    Queue highQueue = initializeQueue(numProcesses, P_HIGH);
+    countProcesses(sortedArray, &l, &m, &h, numProcesses);
+    printf("COUNT PROCESSES: l = %d, m = %d, h = %d\n", l, m, h);
+    Queue lowQueue = initializeQueue(numProcesses, P_LOW, l);
+    Queue medQueue = initializeQueue(numProcesses, P_MEDIUM, m);
+    Queue highQueue = initializeQueue(numProcesses, P_HIGH, h);
     populateQueues(sortedArray, &lowQueue, &medQueue, &highQueue, numProcesses);
     
-    printf("lowQueue:\n");
-    printProcessesInQueue(lowQueue, lowQueue.members);
-    printf("medQueue:\n");
-    printProcessesInQueue(medQueue, medQueue.members);
-    printf("highQueue:\n");
-    printProcessesInQueue(highQueue, highQueue.members);
+    if (l > 0) {
+        printf("lowQueue:\n");
+        printProcessesInQueue(lowQueue, l);
+    }
+    if (m > 0) {
+        printf("medQueue:\n");
+        printProcessesInQueue(medQueue, m);
+    }
+    if (h > 0) {
+        printf("highQueue:\n");
+        printProcessesInQueue(highQueue, h);
+    }
 
-    printf("lowQueue.members = %d\n", lowQueue.members);
+    roundRobinPriority(&lowQueue, &medQueue, &highQueue);
+
+    printTotalAverage(&lowQueue, &medQueue, &highQueue);
+
     freeProcessesInQueue(&lowQueue, lowQueue.members);
-    printf("medQueue.members = %d\n", medQueue.members);
     freeProcessesInQueue(&medQueue, medQueue.members);
-    printf("highQueue.members = %d\n", highQueue.members);
     freeProcessesInQueue(&highQueue, highQueue.members);
-
     freeIntArray(sortedArray, numProcesses);
-
     free(lowQueue.slots);
     free(medQueue.slots);
     free(highQueue.slots);
